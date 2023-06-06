a6f43b8febae8b414bfae33bfce3be9f2f078558
Rashit <gorinnose@yandex.ru>
2023-06-06 20:23:37 +0300

    Добавлены комментарии к задаче 1

--- Seminar_03/task_3_1_lists.py
+++ Seminar_03/task_3_1_lists.py
@@ -1,24 +1,14 @@
 '''
+Задача 1
+
 Дан список повторяющихся элементов. 
 
 Вернуть список с дублирующимися элементами. 
 В результирующем списке не должно быть дубликатов.
 '''
-
-#В большой текстовой строке подсчитать количество встречаемых слов и вернуть 10 самых частых. Не учитывать знаки препинания и регистр символов. За основу возьмите любую статью из википедии или из документации к языку.
-#Создайте словарь со списком вещей для похода в качестве ключа и их массой в качестве значения. Определите какие вещи влезут в рюкзак передав его максимальную грузоподъёмность. Достаточно вернуть один допустимый вариант. *Верните все возможные варианты комплектации рюкзака
-
-
-# 2 - Задайте последовательность чисел. 
-# Напишите программу, которая выведет список неповторяющихся элементов 
-# исходной последовательности. 
-# Не использовать множества.
-# [1,1,1,1,2,2,2,3,3,3,4] -> [1,2,3,4]
-
-
 def input_list_of_nums(output_string):
     '''
-    Данный метод рекурсивно предлгагает пользователю корректно ввести список
+    Данный метод рекурсивно предлагает пользователю корректно ввести список
     Рекурсия, чтоб не использовать цикл
     '''
     result = []
@@ -40,11 +30,16 @@ def input_list_of_nums(output_string):
     return result
 
 def get_list_single_entry_nums_by_set(output_string):
-    return [set(output_string)]
+    '''
+    Возвращает список неповторяющихся элементов исходного списка (без дубликатов)
+    Решение через множество
+    '''
+    return [*set(output_string)]
 
 def get_list_single_entry_nums(input_list):
     '''
-    Возвращает список неповторяющихся элементов исходного списка 
+    Возвращает список неповторяющихся элементов исходного списка (без дубликатов)
+    Решение через списки
     '''
     result = []
     if len(input_list) > 1:
@@ -57,27 +52,28 @@ def get_list_single_entry_nums(input_list):
 
 def get_list_many_entry_nums(input_list):
     '''
-    Возвращает список повторяющихся элементов исходного списка 
+    Возвращает список повторяющихся элементов исходного списка (список дубликатов)
     '''
     result = []
-    # формируем список с неповторяющимися элементами
+    # формируем список с неповторяющимися элементами (без дубликатов)
     list_with_single_entry = get_list_single_entry_nums(input_list)
-    # ищем количество вхождений
+    # ищем количество вхождений и формируем список (математически множество) с повторяющимися элементами 
     if len(input_list) > 1:
         for i, item_searched  in enumerate(list_with_single_entry):
             count_of_entry = 0
             for i, item in enumerate(input_list):
                 if item == item_searched:
                     count_of_entry +=1
-            if count_of_entry > 1 and item_searched not in result:
+            if count_of_entry > 1 and item_searched not in result: #Условие "and item_searched not in result" убирает повторы дубликатов
                 result.append(item_searched) # result.append(input_list[i])
     return result
 
 ### ********* основной код - функция main  ********* 
 output_string = "\nВведите список через пробел и нажмите enter: \n список -> "
 list_of_nums = input_list_of_nums(output_string)
-list_single_entry = get_list_single_entry_nums(list_of_nums)
-list_many_entry = get_list_many_entry_nums(list_of_nums)
-print(f"Результат:\n{list_of_nums}=> \
-        неповторяющиеся {sorted(list_single_entry)} + \
-        повторяющиеся {sorted(list_many_entry)}")
+list_single_entry = get_list_single_entry_nums(list_of_nums)   # В результирующем списке не должно быть *дубликатов* (решение не используя множества)
+# list_single_entry_by_set = [*set(list_of_nums)]                   # решение через множество
+list_many_entry_items = get_list_many_entry_nums(list_of_nums) # Вернуть список с дублирующимися элементами
+print(f"Результат:\n{list_of_nums} => \
+        \n список без дубликатов {sorted(list_single_entry_by_set)} \
+        \n список с дублирующимися элементами {sorted(list_many_entry_items)}")
